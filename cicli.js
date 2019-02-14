c9e50f74bf75ad0f8fa64d4604c2c146479c8d57
MarkoM91 <markomedic91@gmail.com>
2019-02-14 14:57:59 +0100

    document1

---
+++ cicli.js
@@ -0,0 +1,15 @@
+for (var i=0; i < 100; i++)
+
+if ( i%5 == 0){
+   document.getElementById('mul5').innerHTML= i;
+}
+else{
+  document.getElementById('other').innerHTML= i;
+}
+
+if ( i%7 == 0){
+  document.getElementById('mul5').innerHTML= i;
+}
+else{
+ document.getElementById('other').innerHTML= i;
+}
